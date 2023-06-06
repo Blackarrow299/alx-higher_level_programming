04f9c8f4bb822d5b9142b2f014e051497fc853be
Blackarrow299 <yassine.goumni@gmail.com>
2023-06-06 21:04:51 +0100

    stupid string format

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for n in range(0, 100):
     if n == 99:
-        print(f"{n:0>2d}", end="")
+        print("{:0>2d}".format(n), end="")
         continue
-    print(f"{n:0>2d}, ", end="")
+    print("{:0>2d}, ".format(n), end="")
