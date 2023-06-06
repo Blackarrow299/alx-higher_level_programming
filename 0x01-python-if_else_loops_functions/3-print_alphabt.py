f413c20ce07ed777f2d12d61aa1cf91b12884850
Blackarrow299 <yassine.goumni@gmail.com>
2023-06-06 20:53:10 +0100

    stupid /usr/bin/python3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 for chr in range(97, 123):
     if chr == 101 or chr == 113:
         continue
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 for n in range(0, 100):
     if n == 99:
         print(f"{n:0>2d}", end="")
