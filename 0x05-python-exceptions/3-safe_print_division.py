38ce61ffda9ed3da7d678971bf6782fff1da41f4
Blackarrow299 <yassine.goumni@gmail.com>
2023-06-26 21:54:52 +0100

    task 2-3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-def safe_print_division(a, b): 
-    out = 0 
+def safe_print_division(a, b):
+    out = 0
+    print("Inside result: ", end="")
     try:
         out = a / b
     except Exception:
         out = None
     finally:
-        print("Inside result: {}".format(out))
+        print("{}".format(out))
         return out
 
 
