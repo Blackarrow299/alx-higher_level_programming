02a441f454e157e10d2349fc85ba855d8dc08fc0
Blackarrow299 <yassine.goumni@gmail.com>
2023-06-26 21:13:04 +0100

    task 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,11 +4,11 @@ def safe_print_list(my_list=[], x=0):
     try:
         for n in my_list:
             if x <= 0:
-                break;
+                break
             print("{}".format(n), end="")
             i += 1
             x -= 1
         print("")
-    except:
+    except Exception:
         pass
     return i
