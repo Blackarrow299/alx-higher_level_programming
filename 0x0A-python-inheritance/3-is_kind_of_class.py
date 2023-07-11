0c97d5058af33118793d158be59d81a6afeb925f
Blackarrow299 <yassine.goumni@gmail.com>
2023-07-11 22:25:21 +0100

    task 3-6

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" is_kind_of_class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ returns True if the object is an instance of <a_class>,
+
+    Args:
+        obj (class): 
+        a_class (class): 
+
+    Returns:
+        boolean : 
+    """
+    return isinstance(obj, a_class)
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""BaseGeometry"""
+
+
+class BaseGeometry:
+    """empty BaseGeometry Class"""
+    pass
