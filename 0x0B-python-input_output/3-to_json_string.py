ae7276f630305f077ac2896c9137895ef6dbddc9
Blackarrow299 <yassine.goumni@gmail.com>
2023-07-12 21:22:43 +0100

    task 3-4

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""3-to_json_string"""
+import json
+
+
+def to_json_string(my_obj):
+    """object to json"""
+    return json.dump(my_obj)
---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""4-from_json_string"""
+import json
+
+
+def from_json_string(my_str):
+    """json to object"""
+    return json.load(my_str)
