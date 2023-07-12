7712667a105b186c4950df51301ff362351077de
Blackarrow299 <yassine.goumni@gmail.com>
2023-07-12 21:48:24 +0100

    task 7

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -4,6 +4,7 @@ import json
 
 
 def load_from_json_file(filename):
+    """json file content to object"""
     with open(filename, "r") as f:
         text = f.read()
         return json.loads(text)
