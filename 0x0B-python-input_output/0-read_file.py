b3cd0cae89072b74419b95cd8d0c4f201a1fa8e9
Blackarrow299 <yassine.goumni@gmail.com>
2023-07-12 21:04:55 +0100

    task0-1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""0-read_file"""
 def read_file(filename=""):
+    """print file content to stdout"""
     with open(filename, "+r", encoding="UTF-8") as f:
         text = f.read()
         print(text)
---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""1-write_file"""
+
+def write_file(filename="", text=""):
+    """write text to file"""
+    with open(filename, "w") as f:
+        f.write(text)
