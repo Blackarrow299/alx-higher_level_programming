211330e4c19eb932b3fc7182508d81186d9de357
Blackarrow299 <yassine.goumni@gmail.com>
2023-09-12 00:46:46 +0100

    0x0A

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""BaseGeometry"""
+
+
+class BaseGeometry:
+    """Base Geometry class
+    """
+
+    def area(self):
+        """raise Exception
+
+        Raises:
+            Exception: area() is not implemented
+        """
+        raise Exception("area() is not implemented")
